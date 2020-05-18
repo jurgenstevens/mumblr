8c11d74f6f1fa0fd084ef909b5a8db32dafb03a6
Jurgen <jurgen.stevens@yahoo.com>
2020-05-17 23:18:44 -0500

    Installed dotenv and required it to connect to .env file.

--- server.js
+++ server.js
@@ -2,8 +2,10 @@
 // S2: In terminal type 'npm install'
 // S3: In terminal type 'npm install express mongoose cors"
 const express = require("express");
-const mongoose = require("mopngoose");
+const mongoose = require("mongoose");
 const cors = require("cors");
+
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -13,3 +15,15 @@ app.use(express.json());
 
 // this will be the connnection to MongoDB through .env
 const uri = process.env.ATLAS_URI;
+// S5: run "npm i dotenv" in terminal to connect to .env file
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+});
+
+const connection = mongoose.connection;
+connection.once("open", () =>
+  console.log("Mongoose connnection established successfully")
+);
+
+app.listen(port, () => console.log(`The app is running on Port : ${port}`));
