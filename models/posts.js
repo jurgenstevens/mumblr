ccd3b45f0d26b5d1455e0c6aa3bc2ef1c1f23d08
Jurgen <jurgen.stevens@yahoo.com>
2020-05-19 16:34:51 -0500

    Finished and exported Post schema from posts.js models.

--- models/posts.js
+++ models/posts.js
@@ -1,8 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+// schema for the post
 const postSchema = new Schema({
   title: { type: String, required: true },
   post: { type: String, required: true },
   authorname: { type: String, required: true },
 });
+
+// this will be exported to be used for the routes
+const Posts = mongoose.model("Posts", postSchema);
+
+// here we're exporting
+module.export = Posts;
