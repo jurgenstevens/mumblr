1160e2cec4ea12a26b8d249b8528e4286af17d30
Jurgen <jurgen.stevens@yahoo.com>
2020-05-18 19:29:48 -0500

    Post schema has been created.

--- models/posts.js
+++ models/posts.js
@@ -1,1 +1,8 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+  title: { type: String, required: true },
+  post: { type: String, required: true },
+  authorname: { type: String, required: true },
+});
