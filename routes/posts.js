37fe69d809c648a6acaffce5f377a0211ed63217
Jurgen <jurgen.stevens@yahoo.com>
2020-05-19 22:22:22 -0500

    Created the route to GET/FIND post by ID.

--- routes/posts.js
+++ routes/posts.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const Posts = require("../models/posts");
 
 // this will be our first route
-// request to GET ALL of the posts to DISPLAY
+// GET request to get ALL of the posts to DISPLAY
 router.get("/", (req, res) => {
   // we're going to find the post from the schema then create the promise to return our posts in JSON format
   Posts.find()
@@ -14,7 +14,7 @@ router.get("/", (req, res) => {
 });
 // S12: go to MongoDB -> Collections -> Add My Own Data
 
-// request to ADD a new post
+// POST request to ADD a new post
 router.post("/add", (req, res) => {
   const newPost = new Posts({
     title: req.body.title,
@@ -26,6 +26,20 @@ router.post("/add", (req, res) => {
     .then(() => res.json("The new post was added successfully!"))
     .catch((err) => res.status(400).res.json(`Error: ${err}`));
 });
+
+//Request to GET/FIND post by ID
+router.get("/:id", (req, res) => {
+  // we're finding by ID through request, parameters and ID
+  Posts.findById(req.params.id)
+    // creating the promise to return a response and return the article in JSON format
+    .then((article) => res.json(article))
+    // standard error catch
+    .catch((err) => res.status(400).res.json(`Error: ${err}`));
+});
+
+//Request to UPDATE post by ID
+
+//Request to FIND post by ID and DELETE
 
 // S13 don't forget to export the router
 module.exports = router;
