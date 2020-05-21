ed0ad4345053ce39e502d1e5abde402baab785a3
Jurgen <jurgen.stevens@yahoo.com>
2020-05-21 02:06:51 -0500

    Installed bootstrap.

--- mumblr/src/App.js
+++ mumblr/src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
+//S20: Clear all of the files you don't need then install
+// Bootstrap by typing "npm i bootstrap" in the terminal
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Mumblr</h1>
+    </div>
+  );
 }
 
 export default App;
