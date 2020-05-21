9a91f9dbf3c4f995284ba1fd7ef686e6f3748e44
Jurgen <jurgen.stevens@yahoo.com>
2020-05-21 16:51:36 -0500

    Imported bootstrap to App.js and added a button to see if Bootstrap works.

--- mumblr/src/App.js
+++ mumblr/src/App.js
@@ -2,11 +2,14 @@ import React from "react";
 import "./App.css";
 //S20: Clear all of the files you don't need then install
 // Bootstrap by typing "npm i bootstrap" in the terminal
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Mumblr</h1>
+      <br />
+      <button className="btn btn-primary">Click Here</button>
     </div>
   );
 }
