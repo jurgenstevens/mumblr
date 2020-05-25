0283550daa6ce58fe14e3786d757dccfc6977255
Jurgen <jurgen.stevens@yahoo.com>
2020-05-25 16:23:54 -0500

    Changed header image and moved font to center of thought bubble image.

--- mumblr/src/components/layouts/Header.js
+++ mumblr/src/components/layouts/Header.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 const Header = () => {
   return (
     <MainContainer>
-      <h1>mumblr</h1>
+      <h1>mumblr!</h1>
     </MainContainer>
   );
 };
@@ -14,15 +14,16 @@ export default Header;
 // S24: Type 'npm i styled-components' in terminal to install in this component
 // This is the main container using styled-components
 const MainContainer = styled.header`
-  background: url(../../images/mumblrheader.png) no-repeat center/cover;
-  height: 15rem;
+  background: url(../../images/mumblrthoughtheader.png) no-repeat center/cover;
+  height: 20rem;
 
   h1 {
     transform: translate(-50%, -50%);
     color: #000;
     font-weight: 900;
     position: absolute;
-    top: 10%;
-    left: 20%;
+    top: 22%;
+    left: 52%;
+    font-size: 60px;
   }
 `;
--- mumblr/src/components/layouts/Navbar.js
+++ mumblr/src/components/layouts/Navbar.js
@@ -23,7 +23,7 @@ const Navbar = () => {
         </button>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
+          <ul className="navbar-nav ml-auto">
             <li className="nav-item active">
               <a className="nav-link" href="#">
                 Home <span className="sr-only">(current)</span>
