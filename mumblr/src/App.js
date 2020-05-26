6d63c6bb5648f8b7c9c5f441c67119727fb83cd4
Jurgen <jurgen.stevens@yahoo.com>
2020-05-25 22:01:00 -0500

    Created a footer.

--- mumblr/src/App.js
+++ mumblr/src/App.js
@@ -5,12 +5,14 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/layouts/Header";
 import Navbar from "./components/layouts/Navbar";
+import Footer from "./components/layouts/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Navbar />
+      <Footer />
     </div>
   );
 }
---
+++ mumblr/src/components/layouts/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const Footer = () => {
+  return <FooterContainer></FooterContainer>;
+};
+
+export default Footer;
+
+// footer container
+const FooterContainer = styled.footer`
+  background: #383838;
+  height: 4rem;
+`;
