9a1dee0f831dcae51a3cb710c5108679ddc35cad
Jurgen <jurgen.stevens@yahoo.com>
2020-05-25 22:23:23 -0500

    Completed footer and included Copyright.

--- mumblr/src/components/layouts/Footer.js
+++ mumblr/src/components/layouts/Footer.js
@@ -2,14 +2,18 @@ import React from "react";
 import styled from "styled-components";
 
 // S31: Import styled from styled-components and follow the same steps as the Navbar.
-
+// S33: Add the Copyright and current date of the copyright by using the Date method
 const Footer = () => {
-  return <FooterContainer></FooterContainer>;
+  return (
+    <FooterContainer>
+      <span>&copy;{new Date().getFullYear()}All Rights Reserved. Mumblr.</span>
+    </FooterContainer>
+  );
 };
 
 export default Footer;
 
-// footer container
+// S32: Add background color, height, and place it to the bottom at 100%
 const FooterContainer = styled.footer`
   background: var(--navbar-blue);
   height: 4rem;
@@ -17,4 +21,8 @@ const FooterContainer = styled.footer`
   left: 0;
   bottom: 0;
   width: 100%;
+
+  span {
+    color: white;
+  }
 `;
