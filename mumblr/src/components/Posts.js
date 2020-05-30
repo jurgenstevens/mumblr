ccb739821bb96aa01efb3ba51143b8a79ee08876
Jurgen <jurgen.stevens@yahoo.com>
2020-05-30 02:56:26 -0500

    Included S50 instructions for rendering OPs' names.

--- mumblr/src/components/Posts.js
+++ mumblr/src/components/Posts.js
@@ -7,6 +7,7 @@ const Posts = ({ posts }) => {
     // S47: In curly braces within the div, add posts and use the map method with the arrow function with post and key within the parameter of the arrow function
     // S48: Within the arrow function use your HTML tags and within curly braces, the javascript {post.title}
     // S49: Now that the post titles rendered, to render the post's post/article, you must wrap the title, post/article, and author within a div or else you'll get and error
+    // S50: Now include post.authorname under the post/article to render OP's name
     <div>
       Hello from the Posts component!
       {posts.map((post, key) => (
