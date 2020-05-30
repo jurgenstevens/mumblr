14f657dc6441ba4e815929967f7fefc40bf33fc7
Jurgen <jurgen.stevens@yahoo.com>
2020-05-29 23:08:02 -0500

    Wrapped the App component in the Router tags.

--- mumblr/src/index.js
+++ mumblr/src/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// S36: Imported below
+// S36: Imported below and then wrap the App components within the Router tags
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
