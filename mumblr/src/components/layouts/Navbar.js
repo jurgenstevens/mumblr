f3ecdf68e0acb7273e858614ef4f9c88dd05cb5b
Jurgen <jurgen.stevens@yahoo.com>
2020-06-09 00:48:38 -0500

    Changed all of the hrefs to 'to'.

--- mumblr/src/components/layouts/Navbar.js
+++ mumblr/src/components/layouts/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <NavbarContainer>
       <nav className="navbar navbar-expand-lg navbar-dark">
-        <a className="navbar-brand" href="/">
+        <a className="navbar-brand" to="/">
           mumblr.
         </a>
         <button
@@ -29,17 +29,17 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item active">
-              <a className="nav-link" href="/">
+              <a className="nav-link" to="/">
                 Home <span className="sr-only">(current)</span>
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <a className="nav-link" to="#">
                 Link
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <a className="nav-link" to="#">
                 Create Post
               </a>
             </li>
