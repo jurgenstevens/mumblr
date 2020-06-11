522ccfa2a04458197c5b654e4300bfe075d4e3dd
Jurgen <jurgen.stevens@yahoo.com>
2020-06-10 23:50:07 -0500

    Copied and pasted the code for Forms from the boostrap website without instructions.

--- mumblr/src/components/AddPost.js
+++ mumblr/src/components/AddPost.js
@@ -1,23 +1,22 @@
 // S64 Use rfce to set up the AddPost component. For S65 switch to App.js
-// S67: Copy and paste the forms code from the Bootstrap website
 import React from "react";
 
 const AddPost = () => {
   return (<form>
-  <div className="form-group">
+  <div class="form-group">
     <label for="exampleInputEmail1">Email address</label>
-    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
   </div>
-  <div className="form-group">
+  <div class="form-group">
     <label for="exampleInputPassword1">Password</label>
-    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password">
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
   </div>
-  <div className="form-check">
-    <input type="checkbox" className="form-check-input" id="exampleCheck1">
-    <label className="form-check-label" for="exampleCheck1">Check me out</label>
+  <div class="form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
   </div>
-  <button type="submit" className="btn btn-primary">Submit</button>
+  <button type="submit" class="btn btn-primary">Submit</button>
 </form>)
 };
 
