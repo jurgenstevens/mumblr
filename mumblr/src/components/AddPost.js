adbe38d12ec3a3db17ade4bdc89599b6267fc998
Jurgen <jurgen.stevens@yahoo.com>
2020-06-15 14:02:41 -0500

    Fixed placeholders for input fields and added Create A Post header with css using styled-components.

--- mumblr/src/components/AddPost.js
+++ mumblr/src/components/AddPost.js
@@ -1,32 +1,36 @@
-// S64: Use rfce to set up the AddPost component. For S65 switch to App.js
-// S67: Copy and paste the forms code from the Bootstrap website. Change class attribute to className, the for's to htmlFor and add slashes at the ends of inputs.
 import React from "react";
 import styled from "styled-components";
 
+// S64: Use rfce to set up the AddPost component. For S65 switch to App.js
+// S67: Copy and paste the forms code from the Bootstrap website. Change class attribute to className, the for's to htmlFor and add slashes at the ends of inputs.
+// S69: Add the h1 Create Post for the AddPost component and style it below using styled-components
 const AddPost = () => {
   return (
     <AddPostContainer>
-      <form>
-        <div className="form-group">
-          <label htmlFor="authorname">Author</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Author Name"
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="title">Title</label>
-          <input type="text" className="form-control" placeholder="Title" />
-        </div>
-        <div class="form-group">
-          <label htmlFor="post">Post</label>
-          <textarea class="form-control" rows="2"></textarea>
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form>
+      <div className="container">
+        <h1>Create A Post</h1>
+        <form>
+          <div className="form-group">
+            <label htmlFor="authorname">Author</label>
+            <input type="text" className="form-control" placeholder="Author" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="title">Title</label>
+            <input type="text" className="form-control" placeholder="Title" />
+          </div>
+          <div class="form-group">
+            <label htmlFor="post">Post</label>
+            <textarea
+              class="form-control"
+              rows="2"
+              placeholder="Write post here"
+            ></textarea>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
     </AddPostContainer>
   );
 };
@@ -39,4 +43,9 @@ const AddPostContainer = styled.div`
   margin: 3rem auto;
   padding: 4rem;
   width 31.25rem;
+
+  h1 {
+    font-weight: 800;
+    color: #2778e8;
+  }
 `;
