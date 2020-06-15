1e156b6745fd367dcd29987d0213e4c5af340df0
Jurgen <jurgen.stevens@yahoo.com>
2020-06-15 13:15:12 -0500

    Imported styled-components to AddPost component to add css to the form - S68.

--- mumblr/src/components/AddPost.js
+++ mumblr/src/components/AddPost.js
@@ -1,9 +1,11 @@
-// S64 Use rfce to set up the AddPost component. For S65 switch to App.js
-import React from "react";
+// S64: Use rfce to set up the AddPost component. For S65 switch to App.js
 // S67: Copy and paste the forms code from the Bootstrap website. Change class attribute to className, the for's to htmlFor and add slashes at the ends of inputs.
+import React from "react";
+import styled from "styled-components";
+
 const AddPost = () => {
   return (
-    <div>
+    <AddPostContainer>
       <form>
         <div className="form-group">
           <label htmlFor="authorname">Author</label>
@@ -25,8 +27,16 @@ const AddPost = () => {
           Submit
         </button>
       </form>
-    </div>
+    </AddPostContainer>
   );
 };
 
 export default AddPost;
+
+// S68: Import styled components after importing react and continue styling the AddPost component below
+// main container styling
+const AddPostContainer = styled.div`
+  margin: 3rem auto;
+  padding: 4rem;
+  width 31.25rem;
+`;
