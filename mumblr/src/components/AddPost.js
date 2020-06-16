f7a4b21e96e471361bec9e23eb8ed484fd284e90
Jurgen <jurgen.stevens@yahoo.com>
2020-06-15 19:28:27 -0500

    Added the value attribute for each input.

--- mumblr/src/components/AddPost.js
+++ mumblr/src/components/AddPost.js
@@ -1,24 +1,48 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import axios from "axios";
 
 // S64: Use rfce to set up the AddPost component. For S65 switch to App.js
 // S67: Copy and paste the forms code from the Bootstrap website. Change class attribute to className, the for's to htmlFor and add slashes at the ends of inputs.
 // S69: Add the h1 Create Post for the AddPost component and style it below using styled-components
 // S70: To send the data that is entered in the front end to the database, you start off by using hooks. So import useState then use the hook above return for each data input i.e. title, post and authorname
 // S71: Since the state doesn't change when you type something in, you must add the onChange attribute on each input field for each input type
+// S72: In order to send data to the database, first, you must import axios, then in form, use encType attribute. For more info click here https://stackoverflow.com/questions/4526273/what-does-enctype-multipart-form-data-mean
+// S73: Then add the onSubmit attribute and insert the function title for what will change when submit is clicked i.e. changeOnClick
 const AddPost = () => {
   const [title, setTitle] = useState("");
   const [post, setPost] = useState("");
   const [authorname, setAuthorName] = useState("");
+
+  const changeOnClick = (e) => {
+    e.preventDefault();
+    // S74: Create the variable posts that will contain the state (i.e. title, post and authorname)
+    const posts = {
+      title,
+      post,
+      authorname,
+    };
+
+    // S75: The axios will send this data to the database with the post method and the promise
+    axios
+      .post("/posts/add", posts)
+      .then((res) => console.log(res.data))
+      .catch((err) => {
+        console.log(err);
+      });
+    // S76: To clear the fields
+  };
+
   return (
     <AddPostContainer>
       <div className="container">
         <h1>Create A Post</h1>
-        <form>
+        <form onSubmit={changeOnClick} encType="multipart/form-data">
           <div className="form-group">
             <label htmlFor="authorname">Author</label>
             <input
               type="text"
+              value={authorname}
               onChange={(e) => setAuthorName(e.target.value)}
               className="form-control"
               placeholder="Author"
@@ -28,6 +52,7 @@ const AddPost = () => {
             <label htmlFor="title">Title</label>
             <input
               type="text"
+              value={title}
               onChange={(e) => setTitle(e.target.value)}
               className="form-control"
               placeholder="Title"
@@ -36,6 +61,7 @@ const AddPost = () => {
           <div className="form-group">
             <label htmlFor="post">Post</label>
             <textarea
+              value={post}
               onChange={(e) => setPost(e.target.value)}
               className="form-control"
               rows="2"
